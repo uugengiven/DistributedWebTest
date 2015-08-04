27a65fd49765b631580d2eae1171e026829b86ee
Bryan Rittenberry <bryan.rittenberry@ddiworld.com>
2015-08-04 16:46:49 -0400

    Initial commit of server projects

---
+++ Server/src/PerformanceTester.WebApi/Controllers/DataController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DistributedWebTest.Server.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+        [HttpPost]
+        public int SendData()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [HttpPost]
+        public void SendHarData()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
