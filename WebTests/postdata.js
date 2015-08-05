61a8ed4c00d27b4105c0956ed34408b91f5ffdae
uugengiven <lange.john.m@gmail.com>
2015-08-05 15:10:46 -0400

    Add rest api connector as test, working

---
+++ WebTests/postdata.js
@@ -0,0 +1,54 @@
+// postdata.js for posting to the api
+// ==================================
+
+module.exports = {
+    post: function (host, endpoint, data, success) {
+
+
+
+        performRequest(host, endpoint, 'POST', data, success);
+    }
+}
+
+var querystring = require('querystring');
+var http = require('http');
+
+function performRequest(host, endpoint, method, data, success) {
+  var dataString = JSON.stringify(data);
+  var headers = {};
+  
+  if (method == 'GET') {
+    endpoint += '?' + querystring.stringify(data);
+  }
+  else {
+    headers = {
+      'Content-Type': 'application/json',
+      'Content-Length': dataString.length
+    };
+  }
+  var options = {
+    host: host,
+    path: endpoint,
+    method: method,
+    headers: headers
+  };
+
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf-8');
+
+    var responseString = '';
+
+    res.on('data', function(data) {
+      responseString += data;
+    });
+
+    res.on('end', function() {
+      console.log(responseString);
+      var responseObject = JSON.parse(responseString);
+      success(responseObject);
+    });
+  });
+
+  req.write(dataString);
+  req.end();
+}
