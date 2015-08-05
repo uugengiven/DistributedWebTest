9d132398c88d4931d7532c8eeaeff10cb6ffe722
Bryan Rittenberry <bryan.rittenberry@ddiworld.com>
2015-08-05 10:52:54 -0400

    Added the current web api project

---
+++ Server/DistributedWebTest.Server/DistributedWebTest.Server.WebApi/Controllers/DataController.cs
@@ -0,0 +1,23 @@
+﻿using DistributedWebTest.Server.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DistributedWebTest.Server.WebApi.Controllers
+{
+    [RoutePrefix("api/data")]
+    public class DataController : ApiController
+    {
+        // GET api/values
+        [HttpPost]
+        [Route("savetestresults")]
+        public int SaveTestResult(PerformanceTestResult data)
+        {
+            return 1;
+        }
+
+    }
+}
